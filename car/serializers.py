e3ce831d58e0749cf6cd5eb8d99e5a45030c07c1
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2025-02-15 18:43:25 +0200

    solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,44 @@
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField()
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(required=False)
+
+    def validate_horse_powers(self, value):
+        if value < 1 or value > 1914:
+            raise serializers.ValidationError(
+                "Horsepower must be between 1 and 1914."
+            )
+        return value
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.id = validated_data.get("id", instance.id)
+        instance.manufacturer = validated_data.get(
+            "manufacturer",
+            instance.manufacturer
+        )
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get(
+            "horse_powers",
+            instance.horse_powers
+        )
+        instance.is_broken = validated_data.get(
+            "is_broken",
+            instance.is_broken
+        )
+        instance.problem_description = validated_data.get(
+            "problem_description",
+            instance.problem_description
+        )
+        instance.save()
+        return instance
